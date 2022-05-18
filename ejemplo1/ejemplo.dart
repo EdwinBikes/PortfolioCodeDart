240d9a15e5cdb23e84a211d3228f7a1b96481ce4
Edwinbikes <67319437+EdwinBikes@users.noreply.github.com>
2022-05-18 15:30:38 -0500

    Update ejemplo.dart

--- ejemplo1/ejemplo.dart
+++ ejemplo1/ejemplo.dart
@@ -9,3 +9,27 @@ void main() {
     print("$multiplo x  $i  =  $resultado");
   }
 }
+
+
+void main() {
+  int cuantos = 10;
+    
+  imprimirTablas(multiplo,cantidad){
+  
+  for (int i = 1; i <= cantidad; i++) {
+    int resultado = multiplo * i;
+
+    print("$multiplo x  $i  =  $resultado");
+  }
+  }
+  
+  imprimirTablas(1,cuantos);
+  imprimirTablas(2,cuantos);
+  imprimirTablas(3,cuantos);
+  imprimirTablas(4,cuantos);
+  imprimirTablas(5,cuantos);
+  imprimirTablas(6,cuantos);
+  imprimirTablas(7,cuantos);
+  imprimirTablas(8,cuantos);
+  
+}
