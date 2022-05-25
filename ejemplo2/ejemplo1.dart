1ab688367aa551f7aa8ed999ef623891f407d0d1
Edwinbikes <67319437+EdwinBikes@users.noreply.github.com>
2022-05-25 16:00:21 -0500

    implemencion de arreglos

---
+++ ejemplo2/ejemplo1.dart
@@ -0,0 +1,9 @@
+void main() {
+List personas = ["Edwin","Leo","Nicolas","Oscar","Ana","Jorge"];
+  List queSon = ["gordito","españolete","mi bb","pollo","Doña maria","abuelo"];
+  
+  for (int i = 0; i <personas.length;i++) {
+  
+  print(personas[i]+ " es mi " + queSon[i]);
+}
+}
