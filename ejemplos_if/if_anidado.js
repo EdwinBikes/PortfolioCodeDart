f678745f57a020a84c10d12221a4c30a56f7a6bd
Edwinbikes <67319437+EdwinBikes@users.noreply.github.com>
2022-07-06 10:01:36 -0500

    Create if_anidado.js

---
+++ ejemplos_if/if_anidado.js
@@ -0,0 +1,22 @@
+nota = 65
+
+let notaLetra = "";
+if (nota >= 90) {
+  notaLetra = "A";
+} else {
+  if (nota >= 80) {
+    notaLetra = "B";
+  } else {
+    if (nota >= 70) {
+      notaLetra = "C";
+    } else {
+      if (nota >= 60) {
+        notaLetra = "D";
+      } else {
+        (nota < 50);
+        notaLetra = "F";
+      }
+    }
+  }
+}
+console.log("El resultado " + nota +" la nota seria " + notaLetra);
