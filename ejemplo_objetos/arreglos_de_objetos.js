52d4c294defc9916957a657e7be2103776e49ce4
Edwinbikes <67319437+EdwinBikes@users.noreply.github.com>
2022-07-06 11:48:49 -0500

    Create arreglos_de_objetos.js

---
+++ ejemplo_objetos/arreglos_de_objetos.js
@@ -0,0 +1,20 @@
+let nombre1 {
+  nombre: "Edwin",
+  apellido: "Gutierrez",
+  edad: 28,
+  estatura: 170,
+}
+
+let nombre1 {
+  nombre: "Oscar",
+  apellido: "Gutierrez",
+  edad: 20,
+  estatura: 160,
+}
+
+let nombre1 {
+  nombre: "Yir",
+  apellido: "Orozco",
+  edad: 26,
+  estatura: 150,
+}
