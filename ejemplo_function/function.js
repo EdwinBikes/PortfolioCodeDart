fb78a71e64312538ee520f97ba650093fdc8e00a
Edwinbikes <67319437+EdwinBikes@users.noreply.github.com>
2022-07-06 07:31:26 -0500

    funcion de tablas

--- ejemplo_function/function.js
+++ ejemplo_function/function.js
@@ -1,27 +1,8 @@
-function calcularNota(nota) {
-    let notaLetra = "";
-    if (nota >= 90) {
-      notaLetra = "A";
-    } else {
-      if (nota >= 80) {
-        notaLetra = "B";
-      } else {
-        if (nota >= 70) {
-          notaLetra = "C";
-        } else {
-          if (nota >= 60) {
-            notaLetra = "D";
-          } else {
-            (nota < 50);
-            notaLetra = "F";
-          }
-        }
-      }
+function imprimirTabla (base,limite) {
+
+    for (let i = 1; i<= limite; i++) {
+        let resultado = base * i;
+        console.log(base + " x " + i + " = "+ resultado);
     }
-    console.log("El resultado " + nota +" la nota seria " + notaLetra);
-};
-  calcularNota(50);
-  calcularNota(75);
-  calcularNota(63);
-  calcularNota(91);
-  calcularNota(88);
+}
+imprimirTabla(7,6);
